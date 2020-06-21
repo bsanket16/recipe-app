44d85d95897bfe0cd7ec06f5562a9a4221b184ff
sankt <bsankt16@gmail.com>
2020-06-21 17:32:36 +0530

    assignment 3

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,7 +1,85 @@
-import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
-import { Link } from 'react-router-dom';
+import React,{Component} from 'react';
+import {Card,CardImg,CardText,CardTitle,CardBody,Breadcrumb,BreadcrumbItem, Modal,ModalBody,ModalHeader,Button,Label} from "reactstrap"
+import 'font-awesome/css/font-awesome.min.css'
+import "bootstrap/dist/css/bootstrap.min.css"
+import {Link} from 'react-router-dom'
+import {Control,LocalForm,Errors} from 'react-redux-form'       
 
+const required =(val) =>val&&val.length;
+const maxLength = (len)=>(val)=>!(val)||(val.length<=len)
+const minLength = (len)=>(val)=>(val)&&(val.length>=len)
+
+class CommentForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isModalOpen: false,
+        };
+        this.toggleModal = this.toggleModal.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    toggleModal() {
+        this.setState(prevState => ({
+            isModalOpen: !prevState.isModalOpen,
+        }));
+    }
+
+    handleSubmit(values) {
+        this.toggleModal();
+        console.log("Current State is: " + JSON.stringify(values));
+        alert("Current State is: " + JSON.stringify(values));
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <Button outline onClick={this.toggleModal}>
+                    <span className="fa fa-pencil fa-lg"></span> Submit Comment
+                </Button>
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+                    <ModalBody>
+                        <LocalForm onSubmit={values => this.handleSubmit(values)}>
+                            <div className="form-group">
+                                <Label htmlFor="rating">Rating</Label>
+                                <Control.select model=".rating" id="rating" name="rating" 
+                                    className="form-control" defaultValue={1}>
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                    <option>5</option>
+                                </Control.select>
+                            </div>
+                            <div className="form-group">
+                                <Label htmlFor="author">Your Name</Label>
+                                <Control.text model=".author" id="author" name="author"
+                                    className="form-control" placeholder="Your Name"
+                                    validators={{required, minLength: minLength(3), maxLength: maxLength(15)}}
+                                />
+                                <Errors className="text-danger" model=".author" show="touched" 
+                                    messages={{
+                                        required: 'Required. ', 
+                                        minLength: 'Must be greater than 2 characters. ', 
+                                        maxLength: 'Must be 15 characters or less. ',
+                                    }} 
+                                />
+                            </div>
+                            <div className="form-group">
+                                <Label htmlFor="comment">Comment</Label>
+                                <Control.textarea model=".comment" id="comment" name="comment"
+                                    className="form-control" rows="8"
+                                />
+                            </div>
+                            <Button type="submit" color="primary">Submit</Button>
+                        </LocalForm>
+                    </ModalBody>
+                </Modal>
+            </React.Fragment>
+        );
+    }
+}
     function RenderComments({comments}) {
         if (comments == null) {
             return (<div></div>)
@@ -27,7 +105,7 @@ import { Link } from 'react-router-dom';
                 <ul className='list-unstyled'>
                     {cmnts}
                 </ul>
-
+            <CommentForm />
             </div>
         )
     }
