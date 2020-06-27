fc154aec9d52f7eaa539bb0d982fbb3ef6e1d0b5
sankt <bsankt16@gmail.com>
2020-06-27 20:06:26 +0530

    redux actions-1

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,4 +1,3 @@
-import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
 import { LEADERS } from '../shared/leaders';
